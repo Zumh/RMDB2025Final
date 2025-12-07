17e90b800a5b90fd004ac04fae36f1732e506040
n-turco <nicholas.turco@hotmail.com>
2025-12-07 13:38:14 -0500

    implemented the clear button on the login tab

--- BookStore/BookStore/obj/Debug/net8.0-windows/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net8.0-windows/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6de1debee725467869ad66b25048cee5bf6fdf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa4d44de91c3db081b34b4663b2e0be4d72ed72")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
