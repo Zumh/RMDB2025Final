94d8c40ad1aa68b6255f0f0ad540dc9e34e14478
Zumhliansang Lung Ler <17211423+Zumh@users.noreply.github.com>
2025-12-07 10:51:07 -0500

    Status is message at the bottom

--- BookStore/BookStore/obj/Debug/net8.0-windows/BookStore.AssemblyInfo.cs
+++ BookStore/BookStore/obj/Debug/net8.0-windows/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a7612178ca2c40c5131ac2d4a1c6701acc4c94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4d2285994b39be60a58740603eb0285dbd6309")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
